cd179a0fc85aa916911c0ab487f1f312879c809f
cynoxwr <73246691+cynoxwr@users.noreply.github.com>
2021-02-10 19:23:39 +0100

    Add files via upload

---
+++ htdocs/include/pages/promo.php
@@ -0,0 +1,102 @@
+
+<br>
+<main>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://eu-static.yelmocines.es/content/img/ads/202123102010691.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://eu-static.yelmocines.es/content/img/ads/20212417715217.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+</div>
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  </div>
+
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+  <div class="card responsiveCard">
+  <img src="https://eu-static.yelmocines.es/content/img/ads/20211157146301.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>
+</div>
+
+  
+</main>
---
+++ htdocs/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+  <?php include('./include/head.php');?>
+  <?php include('./include/header.php');?>
+<body>
+  <?php include('./include/pages/promo.php');?>
+</body>
+  <?php include('./include/footer.php');?>
+</html>
