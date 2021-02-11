8f827d47f46ede3cdeadfabab1544ad1692203dd
AzulFayos <azul.fayos@gmail.com>
2021-02-11 22:21:57 +0100

    Codigo para corregir

--- htdocs/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="es">
-  <?php include('./include/head.php');?>
-  <?php include('./include/header.php');?>
-<body>
-  <?php include('./include/pages/promo.php');?>
-</body>
-  <?php include('./include/footer.php');?>
-</html>
