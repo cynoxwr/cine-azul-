cc4a346a97357838bc3ce9c22e736bcaeadc5ff1
Almoll <73604748+Almoll@users.noreply.github.com>
2021-02-27 19:02:55 +0100

    27/02/2021 cookies

--- js/js.js
+++ js/js.js
@@ -1,22 +1,52 @@
-{
-/* ésto comprueba la localStorage si ya tiene la variable guardada */
-function compruebaAceptaCookies() {
-  if(localStorage.aceptaCookies == 'true'){
-    cajacookies.style.display = 'none';
+(function () {
+  "use strict";
+
+  var cookieAlert = document.querySelector(".cookiealert");
+  var acceptCookies = document.querySelector(".acceptcookies");
+
+  if (!cookieAlert) {
+     return;
   }
-}
 
-/* aquí guardamos la variable de que se ha
-aceptado el uso de cookies así no mostraremos
-el mensaje de nuevo */
-function aceptarCookies() {
-  localStorage.aceptaCookies = 'true';
-  cajacookies.style.display = 'none';
-}
+  cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
 
-/* ésto se ejecuta cuando la web está cargada */
-$(document).ready(function () {
-  compruebaAceptaCookies();
-});
+  // Show the alert if we cant find the "acceptCookies" cookie
+  if (!getCookie("acceptCookies")) {
+      cookieAlert.classList.add("show");
+  }
 
-src="https://code.jquery.com/jquery-2.1.3.min.js";
+  // When clicking on the agree button, create a 1 year
+  // cookie to remember user's choice and close the banner
+  acceptCookies.addEventListener("click", function () {
+      setCookie("acceptCookies", true, 365);
+      cookieAlert.classList.remove("show");
+
+      // dispatch the accept event
+      window.dispatchEvent(new Event("cookieAlertAccept"))
+  });
+
+  // Cookie functions from w3schools
+  function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) === 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
+})();
+
